83582c22c78fb2b9ebee62f69d90473949bdb21e
Mike Turner <mike.turner@anu.edu.au>
2023-07-26 07:20:52 +0800

    Cells show status of guesses

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
-function Guess({ guess }) {
+function Guess({ guess, answer }) {
+  const result = checkGuess(guess, answer);
+
   return (
     <p className="guess">
       {range(5).map((num) => {
+        const className = guess ? `cell ${result[num].status}` : 'cell';
         return (
-          <span key={num} className="cell">
-            {guess ? guess[num] : undefined}
+          <span key={num} className={className}>
+            {guess ? result[num].letter : undefined}
           </span>
         );
       })}
