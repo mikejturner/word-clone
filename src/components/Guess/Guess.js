a35522752b45cf8302e2158272f2a5def7cbd967
Mike Turner <mike.turner@anu.edu.au>
2023-07-26 06:49:20 +0800

    Show guesses in cells

---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Guess({ guess }) {
+  return (
+    <p className="guess">
+      {guess.split('').map((letter, index) => {
+        return (
+          <span key={index} className="cell">
+            {letter}
+          </span>
+        );
+      })}
+    </p>
+  );
+}
+
+export default Guess;
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import Guess from '../Guess';
+import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 function GuessResults({ guesses }) {
-  console.log(guesses);
-  console.log(typeof guesses);
+  const filledGuesses = [...guesses].splice(0, NUM_OF_GUESSES_ALLOWED);
+  const numBlankGuesses =
+    filledGuesses.length < NUM_OF_GUESSES_ALLOWED
+      ? NUM_OF_GUESSES_ALLOWED - filledGuesses.length
+      : 0;
+  const blankGuesses = range(numBlankGuesses).map((index) => '     ');
+  const guessesToDisplay = [...filledGuesses, ...blankGuesses];
+
   return (
     <div className="guess-results">
-      {guesses.map((guess, index) => {
-        return (
-          <p key={index} className="guess">
-            {guess}
-          </p>
-        );
+      {guessesToDisplay.map((guess, index) => {
+        return <Guess key={index} guess={guess} />;
       })}
     </div>
   );
